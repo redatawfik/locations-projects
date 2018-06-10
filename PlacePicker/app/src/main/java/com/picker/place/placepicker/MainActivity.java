6dc91151bb2c5f5d976e6e57e2c90beba33e6c85
Reda Tawfik <reda.rm148@gmail.com>
2018-06-11 00:08:13 +0200

    ff

---
+++ PlacePicker/app/src/main/java/com/picker/place/placepicker/MainActivity.java
@@ -0,0 +1,66 @@
+package com.picker.place.placepicker;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.gms.common.GooglePlayServicesNotAvailableException;
+import com.google.android.gms.common.GooglePlayServicesRepairableException;
+import com.google.android.gms.location.places.Place;
+import com.google.android.gms.location.places.ui.PlacePicker;
+
+public class MainActivity extends AppCompatActivity {
+
+
+    TextView tv;
+    Button bu;
+
+
+    int PLACE_PICKER_REQUEST = 1;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        tv = findViewById(R.id.txt_view);
+        bu = findViewById(R.id.start_picker_button);
+
+
+    }
+
+    public void startActivity(View view) {
+        PlacePicker.IntentBuilder builder = new PlacePicker.IntentBuilder();
+        Intent intent = null;
+        try {
+            intent = builder.build(this);
+        } catch (GooglePlayServicesRepairableException e) {
+            e.printStackTrace();
+            Toast.makeText(this, e + "", Toast.LENGTH_SHORT).show();
+        } catch (GooglePlayServicesNotAvailableException e) {
+            e.printStackTrace();
+            Toast.makeText(this, e + "", Toast.LENGTH_SHORT).show();
+        }
+
+        startActivityForResult(intent, PLACE_PICKER_REQUEST);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+
+        if (requestCode == PLACE_PICKER_REQUEST) {
+            if (resultCode == RESULT_OK) {
+                Place place = PlacePicker.getPlace(data, this);
+                String toastMsg = String.format("Place: %s" +"\n", place.getName());
+                tv.setText(toastMsg);
+                tv.append(String.valueOf(place.getLatLng()));
+            }
+        }
+    }
+
+}
